d4096401f5d56c821f86e625145356a80bb65441
deepaksama <deepak.sama@gmail.com>
2022-01-22 20:29:33 -0700

    1b. Completed create transaction in Account

--- Account.py
+++ Account.py
@@ -1,9 +1,12 @@
 import hashlib
+from inspect import signature
 import json
 import base64
 
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
+
+#import binascii
 
 class Account:
     # Default balance is 100 if not sent during account creation
@@ -36,9 +39,6 @@ class Account:
         self._balance -= value
 
     def __generate_key_pair(self):
-        # Implement key pair generation logic
-        # Convert them to pem format strings and store in the class attributes already defined
-
         # Generating the private/public key pair
         private_key = rsa.generate_private_key(
             public_exponent=65537,
@@ -56,20 +56,44 @@ class Account:
         )
 
         # Serializing the public key data to show what the file pem data looks like
-        self._ppublic_pem = public_key.public_bytes(
+        self._public_pem = public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
 
+    # Creates pre-hashed mesage
+    def _get_hash(self, message):
+        # Serialize transaction data with keys ordered, and then convert to bytes format
+        hash_string = json.dumps(message, sort_keys=True)
+        encoded_hash_string = hash_string.encode('utf-8')
+
+        # Take sha256 hash of the serialized message, and then convert to bytes format
+        message_hash = hashlib.sha256(encoded_hash_string).hexdigest()
+        encoded_message_hash = message_hash.encode('utf-8')
+
+        return encoded_message_hash
+
     def create_transaction(self, receiver_id, value, tx_metadata=''):
         nonce = self._nonce + 1
-        transaction_message = {'sender': self._id, 'receiver': receiver_id, 'value': value, 'tx_metadata': tx_metadata, 'nonce': nonce}
+        transaction_message = {'sender': self._id, 'receiver': receiver_id,
+                               'value': value, 'tx_metadata': tx_metadata, 'nonce': nonce}
 
-        signature = ''
+        # Create private key object from private key pem
+        private_key = serialization.load_pem_private_key(
+            self._private_pem,
+            password=None, backend=None
+        )
 
-        # Implement digital signature of the hash of the message
+        # Creates pre-hashed mesage
+        encoded_message_hash = self._get_hash(transaction_message)
+
+        # Sign the encoded hash message using private key
+        unformatted_signature = private_key.sign(encoded_message_hash, padding.PSS(mgf=padding.MGF1(
+            hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH), hashes.SHA256())
+
+        # Signature - Base64 Encode and then decode to bytes to get the signature
+        signature = base64.b64encode(unformatted_signature).decode('utf-8')
 
         self._nonce = nonce
-        return {'message': transaction_message, 'signature': signature}
-        
 
+        return {'message': transaction_message, 'signature': signature}
